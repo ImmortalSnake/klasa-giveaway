c3bac9a9958f0bad1d779af966ae2f8c0af61e36
ImmortalSnake <47276574+ImmortalSnake@users.noreply.github.com>
2019-12-31 20:01:46 +0400

    feat: typescript setup

---
+++ src/lib/client.ts
@@ -0,0 +1,9 @@
+import { KlasaClient, KlasaClientOptions } from 'klasa';
+
+export default class GiveawayClient extends KlasaClient {
+
+    public constructor(options: KlasaClientOptions = {}) {
+        super(options);
+    }
+
+}
