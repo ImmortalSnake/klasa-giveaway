1ff34445180ebcf150f7e92dda3c386a979c9950
ImmortalSnake <mashaal2503@gmail.com>
2019-12-31 16:50:21 +0400

    feat: new GiveawayEmbed class

--- src/commands/Giveaways/list.js
+++ src/commands/Giveaways/list.js
@@ -1,4 +1,5 @@
 const { Command, RichDisplay } = require('klasa');
+const GiveawayEmbed = require('../../lib/structures/GiveawayEmbed');
 
 module.exports = class extends Command {
 	constructor(...args) {
@@ -10,12 +11,11 @@ module.exports = class extends Command {
 	async run(msg) {
 		const giveaways = msg.guild.settings.giveaways;
 		if(giveaways.length === 0) return msg.sendLocale('no_running_giveaway', [msg.prefix]);
-		const display = new RichDisplay();
+		const display = new RichDisplay(new GiveawayEmbed(msg));
 		for(let i = 0; i < giveaways.length; i++) {
 			if(i % 8 === 0) {
-				display.addPage(this.client.embed(msg, {
-					description: giveaways.slice(i).slice(0, 8).map(p => `**${i + 1}] ${p.message} :: ${p.title}**\n`)
-				}));
+				display.addPage(template => template
+					.setDescription(giveaways.slice(i).slice(0, 8).map(p => `**${i + 1}] ${p.message} :: ${p.title}**\n`).join('\n')));
 			}
 		}
 		return display.run(await msg.sendLocale('loading'), { filter: (reaction, user) => user === msg.author });
