505d951e3fb84c5c98bd6d054fcb10f953475070
ImmortalSnake <47276574+ImmortalSnake@users.noreply.github.com>
2020-01-01 13:20:03 +0400

    feat: reminder command

---
+++ src/commands/Utiity/reminder.ts
@@ -0,0 +1,25 @@
+import { Command, CommandStore, KlasaMessage } from 'klasa';
+
+export default class extends Command {
+
+	constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			promptLimit: 1,
+			subcommands: true,
+			aliases: ['remind'],
+			description: 'creates a reminder',
+			usage: '<when:time> <text:...str>'
+		});
+	}
+
+	public async set(msg: KlasaMessage, [when, text]: [Date, string]): Promise<KlasaMessage | KlasaMessage[]> {
+		const reminder = await this.client.schedule.create('reminder', when, {
+			data: {
+				user: msg.author.id,
+				text
+			}
+		});
+		return msg.sendLocale('reminder_create', [reminder.id]);
+	}
+
+}
