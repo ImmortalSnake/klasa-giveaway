1b738ff704d8dd39c0aced57eeb81ee7a939a55b
ImmortalSnake <47276574+ImmortalSnake@users.noreply.github.com>
2020-01-03 15:08:33 +0400

    feat: Removed GiveawayCommand

--- src/commands/Giveaways/end.ts
+++ src/commands/Giveaways/end.ts
@@ -1,8 +1,8 @@
-import { CommandStore, KlasaMessage } from 'klasa';
-import GiveawayCommand from '../../lib/base/GiveawayCommand';
+import { CommandStore, KlasaMessage, Command } from 'klasa';
 import { Message } from 'discord.js';
+import GiveawayClient from '../../lib/client';
 
-export default class extends GiveawayCommand {
+export default class extends Command {
 
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
@@ -25,7 +25,7 @@ export default class extends GiveawayCommand {
 		if (!message) throw msg.language.get('GIVEAWAY_NOT_FOUND');
 
 		const { title, wCount } = giveaway.data;
-		await this.client.giveawayManager.finish(message as KlasaMessage, { title, wCount });
+		await (this.client as GiveawayClient).giveawayManager.finish(message as KlasaMessage, { title, wCount });
 		await giveaway.delete();
 		return null;
 	}
