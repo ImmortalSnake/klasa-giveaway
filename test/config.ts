95345af4f407e882f4faaf4c926c8674561684ff
ImmortalSnake <47276574+ImmortalSnake@users.noreply.github.com>
2020-01-15 22:26:44 +0400

    add test folder

---
+++ test/config.ts
@@ -0,0 +1,11 @@
+import { KlasaClientOptions } from 'klasa';
+
+export const config = {
+	owners: ['410806297580011520'],
+	prefix: '..',
+	preserveSettings: false,
+	noPrefixDM: true,
+	giveaway: {
+		maxGiveaways: 2
+	}
+} as KlasaClientOptions;
