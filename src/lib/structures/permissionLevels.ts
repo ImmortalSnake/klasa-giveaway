16a557d90900f9dc9105ab54a0cace1c38ca970f
ImmortalSnake <mashaal2503@gmail.com>
2020-01-15 21:52:35 +0400

    remove giveaway role

--- src/lib/structures/permissionLevels.ts
+++
@@ -1,9 +0,0 @@
-import { PermissionLevels } from 'klasa';
-
-new PermissionLevels()
-// everyone
-	.add(0, () => true)
-// Giveaway role
-	.add(5, ({ guild, member }) => Boolean(guild && member && member.roles.has(guild.settings.get('roles.giveaway') as string)), { fetch: true })
-// Admins
-	.add(6, ({ guild, member }) => Boolean(guild && member && member.permissions.has('ADMINISTRATOR')), { fetch: true });
