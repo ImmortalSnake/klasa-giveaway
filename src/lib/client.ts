1cff0be095d90ebd58082adff236030848cc2256
ImmortalSnake <47276574+ImmortalSnake@users.noreply.github.com>
2020-01-15 21:05:16 +0400

    Add index and client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,15 +1,44 @@
-import { KlasaClient, KlasaClientOptions } from 'klasa';
+import { KlasaClient, KlasaClientOptions, util } from 'klasa';
 import GiveawayManager from './structures/GiveawayManager';
 import './schemas/Guild';
 import './structures/permissionLevels';
+import { join } from 'path';
+import { OPTIONS } from './util/constants';
 
 export default class GiveawayClient extends KlasaClient {
 
-	public giveawayManager: GiveawayManager;
-	public constructor(options: KlasaClientOptions = {}) {
+	public giveawayManager = new GiveawayManager(this);
+	public constructor(options?: KlasaClientOptions) {
 		super(options);
 
-		this.giveawayManager = new GiveawayManager(this);
+		// @ts-ignore
+		this.constructor[KlasaClient.plugin].call(this);
 	}
 
+	public static [KlasaClient.plugin](this: GiveawayClient) {
+		util.mergeDefault(OPTIONS, this.options);
+		this.giveawayManager = new GiveawayManager(this);
+
+		const coreDirectory = join(__dirname, '../');
+
+		// @ts-ignore
+		this.commands.registerCoreDirectory(coreDirectory);
+		// @ts-ignore
+		this.languages.registerCoreDirectory(coreDirectory);
+		// @ts-ignore
+		this.arguments.registerCoreDirectory(coreDirectory);
+		// @ts-ignore
+		this.tasks.registerCoreDirectory(coreDirectory);
+	}
+
+}
+
+
+declare module 'discord.js' {
+	interface ClientOptions {
+		giveaway?: {
+			maxGiveaways: number;
+			requiredPermission: number;
+		};
+	}
 }
