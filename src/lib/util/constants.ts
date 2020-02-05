e0c0e889959023af9081ca681433f514a48d80dd
ImmortalSnake <47276574+ImmortalSnake@users.noreply.github.com>
2020-02-05 19:55:17 +0400

    feat: customiable embeds

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -1,12 +1,18 @@
-export const COLORS = {
-	PRIMARY: '#42f54e'
-};
+import { GiveawayOptions } from '../client';
+import { MessageEmbed } from 'discord.js';
+import Util from './util';
 
 export const OPTIONS = {
 	giveaway: {
 		maxGiveaways: Infinity,
-		requiredPermission: 5
-	}
+		requiredPermission: 5,
+		givewayRunEmbed: (giveaway, language) => new MessageEmbed()
+			.setTitle(giveaway.title)
+			.setColor('#42f54e')
+			.setDescription(language.get('GIVEAWAY_DESCRIPTION', giveaway.winnerCount, Util.ms(giveaway.endsAt - Date.now())))
+			.setFooter(language.get('ENDS_AT'))
+			.setTimestamp(giveaway.endsAt)
+	} as GiveawayOptions
 };
 
 export const Second = 1000;
