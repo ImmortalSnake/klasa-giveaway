2494a7da446d02e910b77aa780fd12b23b203dc3
ImmortalSnake <mashaal2503@gmail.com>
2020-02-05 19:49:32 +0400

    use config.example

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,9 @@
 import { KlasaClient } from 'klasa';
 import { GiveawayClient } from '../src';
-import { config } from './config';
+import { config, prefix } from './config';
 
 KlasaClient.use(GiveawayClient);
 const client = new KlasaClient(config);
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-client.login('NjEzNjQ5Mjc4MzQyMjY2ODg3.XaySJw.4gqd2dXvcH4_W2nSr06SEcrWlk0');
+client.login(prefix);
